b3d7b4e6c1f8221fcef4f54f7b1d660c1ee98e3f
heba-ali2030 <heba.galaa@gmail.com>
2021-02-21 03:13:05 +0200

    add

--- table.py
+++ table.py
@@ -1,16 +1,20 @@
 # multiplication table
+method= input('choose N for normal table and R for reverse table:  ')
 number= int(input("choose a number: "))
-count= 1
-while count <= 12:
-    result= number* count
-    print(number, "X", count, "=", result)
-    count= count + 1
-
-print('This is the reverse of multiplication table')
+if method =='N':
+    count= 1
+    while count <= 12:
+        result= number* count
+        print(number, "X", count, "=", result)
+        count= count + 1
 
 # reverse-multipication table
-count= 12
-while count >= 1:
-    result= number* count
-    print(number, "X", count, "=", result)
-    count= count - 1
+elif method == 'R':
+    count= 12
+    while count >= 1:
+         result= number* count
+         print(number, "X", count, "=", result)
+         count= count - 1
+
+else:
+    print('please choose N or R to complete: ')
