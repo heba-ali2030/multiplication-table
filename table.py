0c8512d66774387f6216cee0e47c356755245515
heba-ali2030 <heba.galaa@gmail.com>
2021-02-21 02:45:56 +0200

    add

--- table.py
+++ table.py
@@ -2,14 +2,15 @@
 number= int(input("choose a number: "))
 count= 1
 while count <= 12:
-    product= number* count
-    print(number, "X", count, "=", product)
+    result= number* count
+    print(number, "X", count, "=", result)
     count= count + 1
 
+print('This is the reverse of multiplication table')
 
 # reverse-multipication table
 count= 12
 while count >= 1:
-    product= number* count
-    print(number, "X", count, "=", product)
+    result= number* count
+    print(number, "X", count, "=", result)
     count= count - 1
